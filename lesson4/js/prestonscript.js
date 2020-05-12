a73064b958e83debf5f9e518812476ca33aa39f9
okaform <Oka17002@byui.edu>
2020-05-12 10:57:04 -0600

    UPdate

--- lesson4/js/prestonscript.js
+++ lesson4/js/prestonscript.js
@@ -1,4 +1,12 @@
 
 function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("hide");
+    let y = document.getElementById("primaryNav").classList.toggle("hide");
+   /* let x = document.getElementById("changeSymbol").innerHTML;
+   
+    let x1 = document.querySelector("changeSymbol");
+    x1.addEventListener('click', ()=> {x = "&#935; Menu"}, false);
+   
+    // if (x = "&#935; Menu") {
+   //     document.getElementById("changeSymbol").innerHTML= "&#9776; Menu";
+   // }*/
 }
