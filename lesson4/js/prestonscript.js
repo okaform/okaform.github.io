2779cc38114f8ddd884238265e9cca49a756c965
okaform <Oka17002@byui.edu>
2020-05-12 13:43:57 -0600

    UPdated

--- lesson4/js/prestonscript.js
+++ lesson4/js/prestonscript.js
@@ -1,4 +1,33 @@
 
+let arrayOfday = ["Sunday","Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let arrayOfMonth = ["January","February","March", "April", "May", "June", "July",
+                        "August","September","October", "November", "December"];
+
+
+let date = new Date();
+let fullYear = date.getFullYear();
+let date2 = date.getDate();
+
+
+    let day;
+    for (let i = 0; i < arrayOfday.length -1; i++) {
+            if (i == date.getDay()) {
+                day = arrayOfday[i];
+                break;
+            }
+    }
+
+    let month;
+    for (let i = 0; i < arrayOfMonth.length; i++) {
+        if (date.getMonth() == i) {
+            month = arrayOfMonth[i];
+            break;
+        }
+}
+
+document.getElementById("currentDate").innerHTML = day + ", " + date2 + " " + month + " " + fullYear + "."; 
+
+
 function toggleMenu() {
     let y = document.getElementById("primaryNav").classList.toggle("hide");
    /* let x = document.getElementById("changeSymbol").innerHTML;
