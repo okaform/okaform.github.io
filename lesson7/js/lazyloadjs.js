cd49cd280b1ffa1ed846d8c148132dc8aed64565
okaform <Oka17002@byui.edu>
2020-06-06 14:58:34 -0600

    update

--- lesson7/js/lazyloadjs.js
+++ lesson7/js/lazyloadjs.js
@@ -1,5 +1,4 @@
 
-
 const imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const loadImages = (image) => {
@@ -35,3 +34,43 @@ else {//load all images if not supported
   });
 }
 
+//testing 
+
+/*
+const imagesToLoad2 = document.querySelectorAll('img[data-srcset]');
+
+const loadImages2 = (image) => {
+  image.setAttribute('srcset', image.getAttribute('data-srcset'));
+  image.onload = () => {
+    image.removeAttribute('data-srcset');
+  };
+};
+
+const imgOptions2 = {
+  threshold: 0.5,
+};
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          imgObserver.unobserve(item.target);
+        }
+      });
+    }, imgOptions);
+    imagesToLoad2.forEach((img) => {
+      imgObserver.observe(img);
+    });
+}
+
+
+
+else {//load all images if not supported
+  imagesToLoad2.forEach((img) => {
+    loadImages(img);
+  });
+}
+
+
+*/
