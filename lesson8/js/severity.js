7a71ec696284e8e7ae47e8d8713636ce611c9274
okaform <Oka17002@byui.edu>
2020-06-12 19:36:27 -0600

    update

--- lesson8/js/severity.js
+++ lesson8/js/severity.js
@@ -3,15 +3,33 @@ function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
 
+
+
+
 function getInfo() {
-    var fullname = document.getElementById("fullname");
+    let fullname = document.getElementById("fullnames").value;
 
-    let email = document.getElementById("email");
+    let email = document.getElementById("emails").value;
 
-    let tel = document.getElementById("tel");
+    let tel = document.getElementById("tels").value;
 
     let caseNumber = Math.floor(Math.random() * 999999);
+
 }
+
+
+function useInfo(fullname, email, tel, caseNumber) {
+    
+    document.getElementById("getName").innerText = fullname;
+
+    document.getElementById("getEmail").innerText = email;
+
+    document.getElementById("getTel").innerText = tel;
+
+    document.getElementById("getCaseNumber").innerText = caseNumber;
+}
+
+
 
 
 
