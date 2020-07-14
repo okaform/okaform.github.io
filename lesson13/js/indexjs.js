d9007ef2beabb87a6723493f745f1d950c133a64
okaform <Oka17002@byui.edu>
2020-07-13 23:38:50 -0600

    updaet

--- lesson13/js/indexjs.js
+++ lesson13/js/indexjs.js
@@ -107,3 +107,68 @@ fetch(rentalOptions)
 
 // This is for the last updated document
 document.getElementById("lastUpdated").innerHTML = document.lastModified;
+
+
+//This is for the 5 day forcast
+
+let date = new Date();
+let arrayOfday = ["Sunday","Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//create an array to store the days and future days
+let arrayOfDayEstimate = [date.getDay() + 1, date.getDay() + 2, date.getDay() + 3, date.getDay() + 4, date.getDay() + 5];
+    
+let remainder;
+//create a loop with an if statement that gets 5 days in advance.
+for (let i = 0; i < arrayOfDayEstimate.length; i++) {
+    remainder = arrayOfDayEstimate[i] % 7;
+        if (remainder == 0) {
+            arrayOfDayEstimate[i] = 0;
+        }
+        else if (remainder == 1) {
+            arrayOfDayEstimate[i] = 1;
+        }
+        else if (remainder == 2) {
+            arrayOfDayEstimate[i] = 2;
+        }
+        else if (remainder == 3) {
+            arrayOfDayEstimate[i] = 3;
+        }
+        else if (remainder == 4) {
+            arrayOfDayEstimate[i] = 4;
+        }
+        else if (remainder == 5) {
+            arrayOfDayEstimate[i] = 5;
+        }
+        else {
+            arrayOfDayEstimate[i] = 6;
+        }
+
+    }
+
+let day1 = document.getElementById("day1").innerHTML = arrayOfday[arrayOfDayEstimate[0]];
+let day2 = document.getElementById("day2").innerHTML = arrayOfday[arrayOfDayEstimate[1]];
+let day3 = document.getElementById("day3").innerHTML = arrayOfday[arrayOfDayEstimate[2]];
+let day4 = document.getElementById("day4").innerHTML = arrayOfday[arrayOfDayEstimate[3]];
+let day5 = document.getElementById("day5").innerHTML = arrayOfday[arrayOfDayEstimate[4]];
+
+const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?id=3530103&APPID=418c04c2294318c72bd18980b93964a7';
+
+fetch(forecastURL)
+    .then((response) => response.json())
+    .then((jsObject3) => {
+
+        console.table(jsObject3);
+        console.log(jsObject3);
+
+        const forecast = jsObject3['list'];
+
+        for(let i = 2; i < forecast.length; i+=8) {
+             const imagesrc = 'https://openweathermap.org/img/w/' + forecast[i].weather[0].icon + '.png';
+             const desc = forecast[i].weather[0].description;
+             const temp = (forecast[i].main.temp - 273.15) * (9 / 5) + 32;
+
+             document.getElementById(`forecast${i}`).innerHTML = temp.toFixed(1) + " &deg;F";
+             document.getElementById(`imgforecast${i}`).setAttribute('src', imagesrc);
+             document.getElementById(`imgforecast${i}`).setAttribute('alt', desc);
+        }
+
+    });
