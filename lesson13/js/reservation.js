1160d556a63e0bedf43b4cf8f4b1be555e123539
okaform <Oka17002@byui.edu>
2020-07-16 22:57:05 -0600

    updatye

--- lesson13/js/reservation.js
+++ lesson13/js/reservation.js
@@ -53,3 +53,36 @@ for (i = 0; i < drp.length; i++) {
 }
 
 
+function getInfo() {
+    localStorage.fullname = document.getElementById("fullnames").value;
+
+    localStorage.email = document.getElementById("emails").value;
+
+    localStorage.tel = document.getElementById("tels").value;
+
+    localStorage.caseNumber = Math.floor(Math.random() * 999999);
+
+    localStorage.pickUpDate = document.querySelector('input[name="pickupdate"]').value;
+
+    localStorage.dropOffDate = document.querySelector('input[name="dropoffdate"]').value;
+
+    localStorage.rentalType = document.querySelector('select[name[towns]').option.value;
+}
+
+function useInfo() {
+    document.getElementById("getName").innerText = localStorage.fullname;
+
+    document.getElementById("getEmail").innerText = localStorage.email;
+
+    document.getElementById("getTel").innerText = localStorage.tel;
+
+    document.getElementById("getCaseNumber").innerText = localStorage.caseNumber;
+
+    document.getElementById("PickupDate").innerText = localStorage.pickUpDate;
+
+    document.getElementById("dropOffDate").innerText =  localStorage.dropOffDate;
+
+    document.getElementById("rentalType").innerText = localStorage.rentalType;
+    
+
+}
