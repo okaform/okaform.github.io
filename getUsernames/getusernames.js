75b945d2e2fd94bd5e8f6a450eb6e5ebbecedeb7
okaform <Oka17002@byui.edu>
2020-09-08 12:59:01 -0600

    update

--- getUsernames/getusernames.js
+++ getUsernames/getusernames.js
@@ -1,20 +1,51 @@
+let threshold = 1000;
 
 
+function getUsernames(threshold) {
 
-function getUsernames() {
-
-    let threshold = 10;
-    //get the url
-    const url = 'https://jsonmock.hackerrank.com/api/article_users?page=<pageNumber>';
-
-    fetch(url)
+    //use the url to get the total number of pages to use in a loop
+    const urlPage = 'https://jsonmock.hackerrank.com/api/article_users?page';
+    fetch(urlPage)
     .then((response) => response.json())
     .then((jsObject) => {
-        console.log(jsObject)
+        let total = jsObject.total;
+       // console.log(total);
 
 
+       //get the url
+        const url = 'https://jsonmock.hackerrank.com/api/article_users?page=';     
+
+       //run a for loop to get the info on all the pages
+       for (let i = 1; i <= total; i++) {
+        const url = 'https://jsonmock.hackerrank.com/api/article_users?page='+`${i}`;
+        fetch(url)
+        .then((response) => response.json())
+        .then((jsObject2) => {
+            //console.log(jsObject2.data);
+            //access the submission count from the data's data
+           for (let j = 0; j < jsObject2.data.length; j++) {
+               let submission_count =  jsObject2.data[j].submission_count;
+               //check if the submission count is greater than the threshold
+                if (submission_count > 500) {
+                   //let p = document.createElement('p');
+                   //p.innerHTML = jsObject2.data[j].username;
+                   //body.appendChild(p);
+                   //document.getElementById("userNames").innerHTML = jsObject2.data[j].username;
+                   console.log(jsObject2.data[j].username);
+                }
+                else {
+                    continue;
+                }
+            }    
+    
+    
+        });
+       }
 
     });
+
+
+
 
 
 
